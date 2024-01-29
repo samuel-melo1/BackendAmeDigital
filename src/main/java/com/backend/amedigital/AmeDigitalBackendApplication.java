83c503aa4bf37403977c02514d6ac799086ec227
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-01-28 23:35:37 -0300

    initial commit

---
+++ src/main/java/com/backend/amedigital/AmeDigitalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.amedigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmeDigitalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmeDigitalBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/amedigital/AmeDigitalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.amedigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmeDigitalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
