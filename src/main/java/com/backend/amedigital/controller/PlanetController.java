e3c408bbac52ff8f9fa066e0759042f5a43fb3e2
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-01-31 23:52:38 -0300

    created planet Controller

---
+++ src/main/java/com/backend/amedigital/controller/PlanetController.java
@@ -0,0 +1,9 @@
+package com.backend.amedigital.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/planet")
+public class PlanetController {
+}
