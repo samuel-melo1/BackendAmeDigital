3b08f18b495ecb1dfb6096b7cfe9d7ed9d39ff4b
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-02-06 23:43:09 -0300

    logical to implemented quantitys aparicoes

--- src/main/java/com/backend/amedigital/AmeDigitalBackendApplication.java
+++ src/main/java/com/backend/amedigital/AmeDigitalBackendApplication.java
@@ -1,13 +1,34 @@
 package com.backend.amedigital;
 
+import com.backend.amedigital.core.model.Planet;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class AmeDigitalBackendApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         SpringApplication.run(AmeDigitalBackendApplication.class, args);
-    }
 
+
+        String url = "https://swapi.dev/api/planets/1";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String json = restTemplate.getForObject(url, String.class);
+
+        List<String> films = new ArrayList<>();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(json);
+        int nomeDoPlaneta = jsonNode.get("films").size();
+
+        System.out.println(nomeDoPlaneta);
+    }
 }
